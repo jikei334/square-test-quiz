3826dea3d4c9e7d51e39ed35f18f2e04b1791e1b
jikei334 <ikimono_lover@yahoo.co.jp>
2025-11-25 22:21:36 +0900

    add authetication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useAuth } from "react-oidc-context";
 import './App.css'
 
 type QuizItem = {
@@ -12,20 +13,55 @@ function App() {
   const [showAnswer, setShowAnswer] = useState(false);
   const API_URL = import.meta.env.VITE_API_URL as string;
 
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+    const logoutUri = import.meta.env.VITE_LOGOUT_URI;
+    const cognitoDomain = import.meta.env.VITE_COGNITO_DOMAIN;
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
   useEffect(() => {
-    fetch(API_URL + "/question")
-      .then((r) => r.json())
-      .then((json: QuizItem[]) => {
-        setList(json);
-        pickNext(json);
-      });
-  }, []);
+    if (auth.isAuthenticated) {
+      const token = auth.user?.access_token;
+      console.log(auth);
+      console.log(token);
+      console.log(`Bearer ${token}`);
+      fetch(API_URL + "/question", {
+          headers: {
+              Authorization: `Bearer ${token}`,
+          }
+      })
+        .then((r) => r.json())
+        .then((json: QuizItem[]) => {
+          setList(json);
+          pickNext(json);
+        });
+    }
+  }, [auth.isAuthenticated]);
 
   const pickNext = (json: QuizItem[] = list) => {
     const item = json[Math.floor(Math.random() * json.length)];
     setCurrent(item);
     setShowAnswer(_ => false);
   };
+
+  if (auth.isLoading) {
+      return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+      return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (!auth.isAuthenticated) {
+    return (
+      <div>
+        <button onClick={() => auth.signinRedirect()}>ログイン</button>
+      </div>
+    );
+  }
 
   if (!current) return <div>読み込み中...</div>;
 
@@ -35,6 +71,7 @@ function App() {
 
   return (
     <div style={{ padding: 20 }}>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
       <h1>クイズ</h1>
       <h2>{current.question}</h2>
 
